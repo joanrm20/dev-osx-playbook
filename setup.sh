2a803df12fc82c95af3a7950ae80df67369ece3f
Jose Ramirez <jo.chicho@gmail.com>
2023-04-02 12:05:04 +0200

    update setuP

--- setup.sh
+++ setup.sh
@@ -1,22 +1,18 @@
 #!/bin/bash
 
-# Install pip if not already installed
-if ! command -v pip &> /dev/null; then
-    sudo easy_install pip
-fi
 
 # Install wheel if not already installed
 if ! command -v wheel &> /dev/null; then
     # Install wheel via pip
     echo "Installing wheel via pip..."
-    pip install wheel
+    pip3 install wheel
 fi
 
 # Install Ansible if not already installed
 if ! command -v ansible &> /dev/null; then
     # Install Ansible via pip
     echo "Installing Ansible via pip..."
-    pip install ansible
+    pip3 install ansible
 fi
 
 
