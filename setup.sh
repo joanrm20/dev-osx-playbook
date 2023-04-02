f8a3acb528ebcdafe1350a05bd7e05742e3b49ac
Jose Ramirez <jo.chicho@gmail.com>
2023-04-02 11:57:59 +0200

    add wheel

--- setup.sh
+++ setup.sh
@@ -5,9 +5,20 @@ if ! command -v pip &> /dev/null; then
     sudo easy_install pip
 fi
 
-# Install Ansible via pip
-echo "Installing Ansible via pip..."
-pip install ansible
+# Install wheel if not already installed
+if ! command -v wheel &> /dev/null; then
+    # Install wheel via pip
+    echo "Installing wheel via pip..."
+    pip install wheel
+fi
+
+# Install Ansible if not already installed
+if ! command -v ansible &> /dev/null; then
+    # Install Ansible via pip
+    echo "Installing Ansible via pip..."
+    pip install ansible
+fi
+
 
 # Confirm Ansible installation
 echo "Verifying Ansible installation..."
